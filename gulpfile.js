d4174c4deda3f49d8ff857490ade5752369a2029
JingeXC <cbb467cp01@163.com>
2017-02-21 17:13:14 +0800

    修改引入模块的方式

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 var gulp = require("gulp");
-var rename = require("gulp-rename");
-var less = require("gulp-less");
-var sass = require("gulp-sass");
-var styl = require("gulp-stylus");
-var connect = require("gulp-connect");
-var livereload = require('gulp-livereload');
+// var rename = require("gulp-rename");
+// var less = require("gulp-less");
+// var sass = require("gulp-sass");
+// var styl = require("gulp-stylus");
+// var connect = require("gulp-connect");
+// var livereload = require('gulp-livereload');
 
-var uglify = require('gulp-uglify-cli');//压缩js
-var imgmin = require('gulp-imagemin');//压缩图片
-var cssmin = require('gulp-cssmin');//压缩css
-var htmlmin = require('gulp-htmlmin');//压缩html
-var processhtml = require('gulp-processhtml');//修改html中引入的文件名
+// var uglify = require('gulp-uglify-cli');//压缩js
+// var imgmin = require('gulp-imagemin');//压缩图片
+// var cssmin = require('gulp-cssmin');//压缩css
+// var htmlmin = require('gulp-htmlmin');//压缩html
+// var processhtml = require('gulp-processhtml');//修改html中引入的文件名
+var $ = require('gulp-load-plugins')();
 
 var fs = require("fs");
 var json = JSON.parse(fs.readFileSync('./package.json'));
@@ -24,7 +25,7 @@ if(cssstyle!=='styl' && cssstyle!=='less' && cssstyle!=='sass' && cssstyle!=='cs
 gulp.task('rename',function(){
 	var i=1;
 	gulp.src('./RAW/*.jpg')
-	.pipe(rename(function(path){
+	.pipe($.rename(function(path){
 		path.basename = i++;
 	}))
 	.pipe(gulp.dest('./images'));
@@ -34,36 +35,36 @@ gulp.task('rename',function(){
 //html
 gulp.task('html', function () {
   gulp.src('./app/*.html')
-    .pipe(livereload());
+    .pipe($.livereload());
 });
 //css
 gulp.task('styl',function(){
 	gulp.src('./app/css/*.styl')
-	.pipe(styl())
+	.pipe($.stylus())
 	.pipe(gulp.dest('./app/css'));
 })
 gulp.task('less',function(){
 	gulp.src('./app/css/*.less')
-	.pipe(less())
+	.pipe($.less())
 	.pipe(gulp.dest('./app/css'));
 })
 gulp.task('sass',function(){
 	gulp.src('./app/css/*.sass')
-	.pipe(sass())
+	.pipe($.sass())
 	.pipe(gulp.dest('./app/css'));
 })
 gulp.task('css',function(){
 	gulp.src('./app/css/*.css')
-	.pipe(livereload());
+	.pipe($.livereload());
 })
 //javascript
 gulp.task('js',function(){
 	gulp.src('./app/js/*.js')
-	.pipe(livereload());
+	.pipe($.livereload());
 })
 //server
 gulp.task('server',function(){
-	connect.server({
+	$.connect.server({
 		root:'./app',
 		livereload:true,
 		port:8320
@@ -75,7 +76,7 @@ gulp.task('watch',function(){
 	gulp.watch(['./app/css/*.'+cssstyle],[cssstyle]);
 	gulp.watch(['./app/css/*.css'],['css']);
 	gulp.watch(['./app/js/*.js'],['js']);
-	livereload.listen();
+	$.livereload.listen();
 })
 
 
@@ -85,28 +86,28 @@ gulp.task('watch',function(){
 //压缩js文件
 gulp.task('minifyjs',function(){
 	gulp.src('./app/js/*js')
-	.pipe(uglify())
-	.pipe(rename({suffix:'.min'}))
+	.pipe($.uglify())
+	.pipe($.rename({suffix:'.min'}))
 	.pipe(gulp.dest('dist/js'));
 });
 //压缩图片
 gulp.task('imgmin',function(){
 	gulp.src('./app/images/*')
-	.pipe(imgmin())
+	.pipe($.imagemin())
 	.pipe(gulp.dest('dist/images'));
 })
 //压缩Css
 gulp.task('cssmin',function(){
 	gulp.src('./app/css/*.css')
-	.pipe(cssmin())
-	.pipe(rename({suffix:'.min'}))
+	.pipe($.cssmin())
+	.pipe($.rename({suffix:'.min'}))
 	.pipe(gulp.dest('dist/css'));
 })
 //压缩html,修改html引入文件名
 gulp.task('htmlmin',function(){
 	gulp.src('./app/*.html')
-	.pipe(processhtml())
-	.pipe(htmlmin({collapseWhitespace:true}))
+	.pipe($.processhtml())
+	.pipe($.htmlmin({collapseWhitespace:true}))
 	.pipe(gulp.dest('dist'));
 })
 
